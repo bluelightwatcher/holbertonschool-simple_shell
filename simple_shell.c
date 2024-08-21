109d26047d21649a29a1f3ff00b9f0a35f1b97a8
bluelightwatcher <9550@holbertonstudents.com>
2024-08-21 16:40:15 +0200

    Main function of the simple shell

---
+++ simple_shell.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * main - program recreates the basic shell
+ * @argc: is the number or arguments passed to the program
+ * @argv: is a string of command typed by the user
+ * @environ: is a pointer to the program variable environment
+ * Return: 0 on sucess 1 on faillure
+ */
+int main(int argc, char **argv, char **environ)
+{
+	char *input = NULL;
+	int interactive = isatty(STDIN_FILENO);
+	char **args = NULL;
+	static char *program_name;
+
+	(void)argc;
+	program_name = argv[0];
+	if (interactive)
+	{
+		while (1)
+		{
+			display_prompt();
+			input = process_input();
+			if (input == NULL)
+			{
+				printf("\n");
+				break;
+			}
+			args = token_args(input);
+			if (args == NULL)
+			{
+				free(input);
+				continue;
+			}
+			execute_command(program_name, args, environ);
+			free(input);
+			free(args);
+		}
+	}
+	else
+	{
+		non_interactive(program_name, environ);
+	}
+	return (0);
+}
