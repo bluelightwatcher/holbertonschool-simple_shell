bdd7dbb1b0571fd81058456075d8bc5bcfcbd654
bluelightwatcher <9550@holbertonstudents.com>
2024-08-21 16:42:01 +0200

    function tokenizes user input

---
+++ token_args.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * token_args - tokenizes user input into args
+ * @input: is the string storing the stdin
+ * Return: pointer to args[]
+ */
+
+char **token_args(char *input)
+{
+	char **args = malloc(BUFFER_SIZE * sizeof(char *));
+	char *token;
+	int i = 0;
+
+	if (args == NULL)
+	{
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(input, " ");
+	while (token != NULL)
+	{
+		if (strlen(token) > 0)
+		{
+			args[i] = token;
+			i++;
+		}
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+
+	if (i == 0)
+	{
+		free(args);
+		return (NULL);
+	}
+
+	return (args);
+}
+
