db7de1bb608e105247f8530cc0b617a327776b44
bluelightwatcher <9550@holbertonstudents.com>
2024-08-21 16:41:32 +0200

    Programm display prompt

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * display_prompt - displays prompt with no latency using fflush
+ * Return: void
+ */
+
+void display_prompt(void)
+{
+	printf("$ ");
+	fflush(stdout);
+}
